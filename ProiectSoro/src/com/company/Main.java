0e8fc080da1db1129724756cf51063ddf73521c5
rifrafi <ra_rares92@yahoo.com>
2020-06-03 20:52:28 +0300

    Deleted commented c ode.

--- ProiectSoro/src/com/company/Main.java
+++ ProiectSoro/src/com/company/Main.java
@@ -16,25 +16,5 @@ public class Main {
         String dest="C:\\Users\\Rares Sarbu\\Desktop\\SpanacOut.txt";
         FilesOperation.combine(source, dest);
 
-//        final Formatter y;
-//        try {
-//            y = new Formatter("C:\\Users\\Rares\\Desktop\\Aplicatia Spanacului\\FisierulMinune.txt");
-//            System.out.println("You created a file!");
-//        } catch (Exception e) {
-//            System.out.println("Cate o exceptie pe ici colo...");
-//        }
-//        Main firstAttempt = new Main();
-//        firstAttempt.copyFileFromSourceToDestination("C:\\Users\\Rares\\Desktop\\Aplicatia Spanacului\\Spanac (0).txt", "C:\\Users\\Rares\\Desktop\\Aplicatia Spanacului\\FisierulMinune.txt");
-//        firstAttempt.copyFileFromSourceToDestination("C:\\Users\\Rares\\Desktop\\Aplicatia Spanacului\\Spanac (1).txt", "C:\\Users\\Rares\\Desktop\\Aplicatia Spanacului\\FisierulMinune.txt");
-//        firstAttempt.copyFileFromSourceToDestination("C:\\Users\\Rares\\Desktop\\Aplicatia Spanacului\\Spanac (2).txt", "C:\\Users\\Rares\\Desktop\\Aplicatia Spanacului\\FisierulMinune.txt");
-//        firstAttempt.copyFileFromSourceToDestination("C:\\Users\\Rares\\Desktop\\Aplicatia Spanacului\\Spanac (3).txt", "C:\\Users\\Rares\\Desktop\\Aplicatia Spanacului\\FisierulMinune.txt");
-//        firstAttempt.copyFileFromSourceToDestination("C:\\Users\\Rares\\Desktop\\Aplicatia Spanacului\\Spanac (4).txt", "C:\\Users\\Rares\\Desktop\\Aplicatia Spanacului\\FisierulMinune.txt");
-//        firstAttempt.copyFileFromSourceToDestination("C:\\Users\\Rares\\Desktop\\Aplicatia Spanacului\\Spanac (5).txt", "C:\\Users\\Rares\\Desktop\\Aplicatia Spanacului\\FisierulMinune.txt");
-//        firstAttempt.copyFileFromSourceToDestination("C:\\Users\\Rares\\Desktop\\Aplicatia Spanacului\\Spanac (6).txt", "C:\\Users\\Rares\\Desktop\\Aplicatia Spanacului\\FisierulMinune.txt");
-//        firstAttempt.copyFileFromSourceToDestination("C:\\Users\\Rares\\Desktop\\Aplicatia Spanacului\\Spanac (7).txt", "C:\\Users\\Rares\\Desktop\\Aplicatia Spanacului\\FisierulMinune.txt");
-//        firstAttempt.copyFileFromSourceToDestination("C:\\Users\\Rares\\Desktop\\Aplicatia Spanacului\\Spanac (8).txt", "C:\\Users\\Rares\\Desktop\\Aplicatia Spanacului\\FisierulMinune.txt");
-//        firstAttempt.copyFileFromSourceToDestination("C:\\Users\\Rares\\Desktop\\Aplicatia Spanacului\\Spanac (9).txt", "C:\\Users\\Rares\\Desktop\\Aplicatia Spanacului\\FisierulMinune.txt");
-
-
     }
 }
