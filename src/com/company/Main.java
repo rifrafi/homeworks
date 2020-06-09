44dfc964b762b5d62a8e994eb37b84e3b4350ed1
rifrafi <66382039+rifrafi@users.noreply.github.com>
2020-06-09 21:25:37 +0300

    Cam atat am reusit... Acum ma chinui cu ordinea alfabetica...

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,54 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
 public class Main {
 
     public static void main(String[] args) {
+        ArrayList<Person> goodPeople = new ArrayList<>();
+        Person CojocaruNicolae = new Person("Cojocaru", "Nicolae", 1988, 12, 11,
+                "Barbat", "Tamplar");
+        Person PopescuMariana = new Person("Popescu", "Mariana", 1920, 9, 13,
+                "Femeie", "Contabil");
+        Person CostacheMircea = new Person("Costache", "Mircea", 2004, 10, 14,
+                "Barbat", "Elev");
+        Person BaciuIonel = new Person("Baciu", "Ionel", 2010, 2, 24,
+                "Barbat", "Elev");
+        Person BerneaErnest = new Person("Bernea", "Ernest", 2011, 10, 14,
+                "Barbat", "Elev");
+        Person GrigorescuLucian = new Person("Grigorescu", "Lucian", 2000, 7, 9,
+                "Barbat", "Student");
+        Person BobeleaMarghioala = new Person("Bobelea", "Marghioala", 1993, 12, 12,
+                "Femeie", "Notar");
+        Person TanaseAndreea = new Person("Tanase", "Andreea", 1950, 6, 10,
+                "Femeie", "Sudor");
+        Person LupuCristian = new Person("Lupu", "Cristian", 1999, 1, 19,
+                "Barbat", "Medic");
+        Person LupuIonut = new Person("Lupu", "Ionut", 1996, 2, 20,
+                "Barbat", "Instalator");
+        goodPeople.add(CojocaruNicolae);
+        goodPeople.add(PopescuMariana);
+        goodPeople.add(CostacheMircea);
+        goodPeople.add(BaciuIonel);
+        goodPeople.add(BerneaErnest);
+        goodPeople.add(GrigorescuLucian);
+        goodPeople.add(BobeleaMarghioala);
+        goodPeople.add(TanaseAndreea);
+        goodPeople.add(LupuCristian);
+        goodPeople.add(LupuIonut);
+
+            
 
 
+        for (Person people:goodPeople) {
+            people.printPersonCharacteristics();
+        }
 
 
     }
+
+
 }
+
--- src/com/company/Person.java
+++ src/com/company/Person.java
@@ -1,4 +1,65 @@
 package com.company;
 
+import java.time.LocalDate;
+import java.time.Period;
+
+
 public class Person {
+    int age;
+
+
+    String profession;
+    String gender;
+    String firstName;
+    String lastName;
+
+    LocalDate dateOfBirth;
+    LocalDate Present;
+
+    //Constructors
+
+    public Person(String personFirstName, String personLastName, int yearOfBirth, int monthOfBirth, int dayOfBirth, String personGender, String personProfession) {
+        this.firstName = personFirstName;
+        this.lastName = personLastName;
+        this.Present = LocalDate.now();
+        this.dateOfBirth = LocalDate.of(yearOfBirth, monthOfBirth, dayOfBirth);
+        this.age = Period.between(dateOfBirth, Present).getYears();
+        this.gender = personGender;
+        this.profession = personProfession;
+    }
+    //Getters
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public String getProfession() {
+        return profession;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void printPersonCharacteristics() {
+        System.out.println("Numele complet: " + getFirstName() + " " + getLastName());
+        System.out.println("Varsta: " + getAge());
+        System.out.println("Gen: " + getGender());
+        System.out.println("Profesia: " + getProfession());
+        if(age <18){
+            System.out.println("Persoana este minora!");
+        }else {
+            System.out.println("Persoana este majora!");
+        }
+        System.out.println();
+        System.out.println();
+    }
 }
